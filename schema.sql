e6dc1b6a5360ff3bbda749667614aba6ea931868
Sarthak Jagetia <sarthakjagetia@Sarthaks-MacBook-Pro.local>
2017-10-25 18:30:28 -0400

    Adding album functionality

---
+++ schema.sql
@@ -0,0 +1,89 @@
+CREATE DATABASE photoshare;
+USE photoshare;
+
+
+-- CREATE USER TABLE
+CREATE TABLE Users (
+    user_id INT NOT NULL AUTO_INCREMENT,
+    gender VARCHAR(6),
+    email VARCHAR(255) NOT NULL UNIQUE,
+    password VARCHAR(40) NOT NULL,
+    dob DATE NOT NULL,
+    hometown VARCHAR(40),
+    fname VARCHAR(40) NOT NULL,
+    lname VARCHAR(40) NOT NULL,
+    PRIMARY KEY (user_id)
+);
+
+CREATE TABLE Albums(
+	album_id INT NOT NULL AUTO_INCREMENT,
+	Name VARCHAR(40) NOT NULL,
+	date_of_creation DATETIME DEFAULT CURRENT_TIMESTAMP,
+	user_id INT NOT NULL,
+	PRIMARY KEY (album_id),
+	FOREIGN KEY (user_id) REFERENCES Users(user_id) ON DELETE CASCADE
+);
+
+-- CREATE Photo TABLE (include photo entity and 'contains' relationship)
+CREATE TABLE Pictures(
+	picture_id INT NOT NULL AUTO_INCREMENT,
+  user_id INT,
+	caption VARCHAR(200),
+	imgdata BLOB,
+	album_id INT NOT NULL,
+	PRIMARY KEY (picture_id),
+  FOREIGN KEY (user_id) REFERENCES Users (user_id),
+	FOREIGN KEY (album_id) REFERENCES Albums(album_id) ON DELETE CASCADE
+);
+
+-- CREATE FRIENDSHIP TABLE
+CREATE TABLE Friendship(
+	UID1 INT NOT NULL,
+	UID2 INT NOT NULL,
+	PRIMARY KEY(UID1, UID2),
+	FOREIGN KEY (UID1) REFERENCES Users (user_id) ON DELETE CASCADE,
+	FOREIGN KEY (UID2) REFERENCES Users (user_id) ON DELETE CASCADE
+);
+
+
+-- CREATE Comment TABLE (include comment entity and 'comment' relationship)
+CREATE TABLE Comments(
+	comment_id INT NOT NULL AUTO_INCREMENT,
+	text TEXT NOT NULL,
+	date DATETIME DEFAULT CURRENT_TIMESTAMP,
+	user_id INT NOT NULL,
+	picture_id INT NOT NULL,
+	PRIMARY KEY (comment_id),
+	FOREIGN KEY (user_id) REFERENCES Users (user_id) ON DELETE CASCADE,
+	FOREIGN KEY (picture_id) REFERENCES Pictures (picture_id) ON DELETE CASCADE
+);
+
+-- CREATE THE LIKETABLE. WE CAN'T name it LIKE
+CREATE TABLE Likes(
+	user_id INT NOT NULL,
+	picture_id INT NOT NULL,
+	date_of_like DATETIME DEFAULT CURRENT_TIMESTAMP,
+  PRIMARY KEY (picture_id),
+  UNIQUE (picture_id, user_id),
+	FOREIGN KEY (user_id) REFERENCES Users (user_id) ON DELETE CASCADE,
+	FOREIGN KEY (picture_id) REFERENCES Pictures (picture_id) ON DELETE CASCADE
+);
+
+
+-- CREATE Tag TABLE
+CREATE TABLE Tags (
+  tag_id INT NOT NULL AUTO_INCREMENT,
+  tag_word VARCHAR(30),
+  PRIMARY KEY (tag_id)
+);
+
+-- CREATE Associate Table
+CREATE TABLE Has_tag(
+	picture_id INT NOT NULL,
+	tag_id INT NOT NULL,
+  PRIMARY KEY (tag_id),
+	FOREIGN KEY (tag_id) REFERENCES Tags (tag_id)  ON DELETE CASCADE,
+	FOREIGN KEY (picture_id) REFERENCES Pictures (picture_id) ON DELETE CASCADE
+);
+
+
